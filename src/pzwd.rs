efdacdfa96533dc22bdf21047fcc19f18285b0a3
Diogo <diogo6204@gmail.com>
2024-08-16 16:08:15 +0100

    removed colour

--- src/pzwd.rs
+++ src/pzwd.rs
@@ -11,17 +11,17 @@ pub(crate) struct Args {
     min_symbols: usize,
 }
 
-
+// NOTE: using ANSI escape codes might not work for terminal emulators that override default colors.
 pub(crate) fn interactive() -> anyhow::Result<String> {
     let mut size_str: String = Default::default();
     let mut min_symbols_str: String = Default::default();
 
-    yellow_bold!("Password len: ");
+    print!("\x1b[1;33mPassword len:\x1b[1;0m ");
     stdout().flush()?; // necessary, since for performance reasons, because stdout it is line-buffered
     stdin().read_line(&mut size_str)?;
     let size = size_str.trim().parse::<usize>()?;
 
-    yellow_bold!("Minimum symbols: ");
+    print!("\x1b[1;33mMinimum symbols:\x1b[1;0m ");
     stdout().flush()?;
     stdin().read_line(&mut min_symbols_str)?;
     let min_symbols = min_symbols_str.trim().parse::<usize>()?;
